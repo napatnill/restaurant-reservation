031bcf577c2d263101cf4554e0ad4abef5ee92f2
napatnill <napatnill.nn@gmail.com>
2025-04-18 07:25:54 +0700

    fix: post response status code

--- controllers/reservations.js
+++ controllers/reservations.js
@@ -135,7 +135,7 @@ exports.addReservation = async (req, res, next) => {
 
         // Proceed to create
         const reservation = await Reservation.create(req.body);
-        res.status(200).json({success: true, data: reservation});
+        res.status(201).json({success: true, data: reservation});
         
     } catch (error) {
         console.log(error.message);
